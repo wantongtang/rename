04e8325c857606de0abcba2296ba3b928113ead0
wantongtang <1048517841@qq.com>
2018-05-23 01:27:39 +0000

    fix bug

--- rename.py
+++ rename.py
@@ -14,10 +14,10 @@ dst_path="../cry/"
 def file_rename(src_path,dst_path):
 	for root,dirs,files in os.walk(src_path):
 		k = 0
-		home_name = random.sample(string.ascii_letters + string.digits, 8)).lower()
+		home_name = ''.join(random.sample(string.ascii_letters + string.digits, 8)).lower()
 		for single_file in files:
 			src_file = ("%s/%s"%(root,single_file))
-			dst_file  = dst_path +  ''.join(home_name+"_nohash_"+str(k)+".wav"
+			dst_file  = dst_path +  home_name+"_nohash_"+str(k)+".wav"
 			k+=1
 			os.system("cp -rf %s  %s "%(src_file,dst_file))
 
